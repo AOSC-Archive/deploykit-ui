bf157e37a8430dad15c0642b0e8c473b7f30109f
liushuyu <liushuyu011@gmail.com>
2022-08-03 17:02:56 -0600

    views/errorview: implement the buttons

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,8 +1,12 @@
 <script>
+import DKBottomActions from "@/components/DKBottomActions.vue";
+import DKBottomRightButtons from "@/components/DKBottomRightButtons.vue";
+
 export default {
   props: {
     message: String,
   },
+  components: { DKBottomActions, DKBottomRightButtons },
 };
 </script>
 
@@ -12,4 +16,10 @@ export default {
     <p>Installer has encountered an error.</p>
     <p class="error-msg">{{ message }}</p>
   </div>
+  <DKBottomActions>
+    <DKBottomRightButtons>
+      <button class="button" @click="$router.replace('/')">Retry</button>
+      <button class="button" @click="proceed">Exit</button>
+    </DKBottomRightButtons>
+  </DKBottomActions>
 </template>
