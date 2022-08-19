eced4049c3e35fae7e2c26851f45957615fb8043
liushuyu <liushuyu011@gmail.com>
2022-08-18 21:15:48 -0600

    component/dklogo: when using an anastrophe-style ...
    ... language, the logo component will invert its layout

--- src/components/DKLogo.vue
+++ src/components/DKLogo.vue
@@ -1,7 +1,11 @@
 <script>
 const CJK_LANG = ["zh-CN", "zh-TW", "ja"];
+const INV_LANG = [""];
 export default {
-  methods: {
+  computed: {
+    is_inverted: function () {
+      return INV_LANG.includes(this.$i18n.locale);
+    },
     cjk_quirk: function () {
       return CJK_LANG.includes(this.$i18n.locale) ? "cjk" : "";
     },
@@ -19,11 +23,17 @@ export default {
       width="32"
       height="32"
     />
-    <div style="line-height: 1">
+    <div style="line-height: 1" v-if="!is_inverted">
       <span style="font-size: 0.82rem">{{ $t("logo-p1") }}</span>
-      <h2 :class="cjk_quirk()" style="font-size: 1.25rem">
+      <h2 :class="cjk_quirk" style="font-size: 1.25rem">
         {{ $t("logo-p2") }}
       </h2>
+    </div>
+    <div style="line-height: 1" v-else>
+      <h2 style="font-size: 1.25rem">
+        {{ $t("logo-p2") }}
+      </h2>
+      <span style="font-size: 0.82rem">{{ $t("logo-p1") }}</span>
     </div>
   </div>
 </template>
